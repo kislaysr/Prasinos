eeecf7c456dd9de6904d034f6483ffad65567dad
kislaysr <kislay.srivastava2015@vit.ac.in>
2017-04-14 01:03:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kislay/prasinos/activity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kislay.prasinos.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.kislay.prasinos.R;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
